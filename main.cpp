6bb157bfa1941dde8aef448cb9a788a5f9733e48
kashviakacoder <kashvia0906@gmail.com>
2025-07-20 13:03:26 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-int main() {
-    int num, digit, sum = 0;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    while (num > 0) {
-        digit = num % 10;    // get last digit
-        sum += digit;        // add to sum
-        num /= 10;           // remove last digit
+int main()
+{
+    int num,i, isPrime=1;
+    printf("enter number");
+    scanf("%d",&num);
+    if (num<=1){
+        isPrime=0;
     }
-
-    printf("Sum of digits = %d\n", sum);
+    for(i=2; i<num;i++){
+        if(num%i==0){
+            isPrime=0;
+        }
+    }
+    if(isPrime)
+        printf("%d is a Prime number.\n",num);
+    else
+        printf("%d is NOt a Prime number.\n",num);
     return 0;
 }
